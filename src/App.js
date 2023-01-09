8bbd8a49560fa91116fe0824053955f0b63e9b62
Yuri <johnpaul.dubecq@hotmail.fr>
2023-01-09 20:30:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  console.log("fsdfsd");
+
+  return (
+    <div className="App">
+      <div id="page">
+        <div id="topPart" className="part">
+          sss
+        </div>
+        <div id="bottomPart" className="part">
+          aaa
+        </div>
+      </div>
+    </div>
+  );
+}
